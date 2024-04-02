39ba0eace93d9a98feac77ceb183692dc862051b
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-04-02 17:02:48 +0900

    Fix: User클래스추가

--- oop-metro/src/main/java/model/vo/user/User.java
+++ oop-metro/src/main/java/model/vo/user/User.java
@@ -1,4 +1,19 @@
 package model.vo.user;
 
+import model.vo.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    public List<Card> saveUser() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(0, "정예진", 24, "climate"));
+        cardList.add(new Card(1, "나지영", 24, "prepaid"));
+        cardList.add(new Card(2, "박수빈", 25, "deferred"));
+
+        return cardList;
+    }
+
 }
--- oop-metro/src/main/java/run/App.java
+++ oop-metro/src/main/java/run/App.java
@@ -3,15 +3,15 @@ package run;
 import model.vo.Charger;
 import model.vo.Gate;
 import model.vo.card.Card;
+import model.vo.user.User;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Card[] cards = new Card[3];
-        cards[0] = new Card(0, "정예진", 24, "climate");
-        cards[1] = new Card(1, "나지영", 24, "prepaid");
-        cards[2] = new Card(2, "박수빈", 25, "deferred");
+        User user = new User();
+        List<Card> userList = user.saveUser();
 
         Scanner sc = new Scanner(System.in);
         Gate gate = new Gate();
@@ -25,14 +25,14 @@ public class App {
         switch (selected) {
             case 1 : gate.menu(); break;
             case 2 :
-                charger.menu(cards[userId], userId);
-                System.out.println(cards[userId].getName() +"님의 카드현황 입니다.");
-                if (cards[userId].getCard().equals("climate")) {
-                    System.out.println(cards[userId].getClimateCards()[userId]);
-                } else if (cards[userId].getCard().equals("prepaid")) {
-                    System.out.println(cards[userId].getPrepaidCards()[userId]);
-                } else if (cards[userId].getCard().equals("deferred")) {
-                    System.out.println(cards[userId].getDeferredCards()[userId]);
+                charger.menu(userList.get(userId), userId);
+                System.out.println(userList.get(userId).getName() +"님의 카드현황 입니다.");
+                if (userList.get(userId).getCard().equals("climate")) {
+                    System.out.println(userList.get(userId).getClimateCards()[userId]);
+                } else if (userList.get(userId).getCard().equals("prepaid")) {
+                    System.out.println(userList.get(userId).getPrepaidCards()[userId]);
+                } else if (userList.get(userId).getCard().equals("deferred")) {
+                    System.out.println(userList.get(userId).getDeferredCards()[userId]);
                 }
         }
     }
